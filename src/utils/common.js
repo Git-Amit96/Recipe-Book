ba6b23d506c0ef50733a83a122579894d56baab1
Git-Amit96 <amitsaini962561@gmail.com>
2024-12-09 18:05:23 +0530

    Bug Fixed

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,2 +1,1 @@
 export const LOGO_Url= "https://images.vexels.com/media/users/3/224164/isolated/lists/841be8b857448d1e2d59637111a8b4fa-chef-hat-clothing-logo.png";
-export const Recipes= "https://api.spoonacular.com/recipes/complexSearch?apiKey=458b079df6434e21987bc39a622c2cdf";
